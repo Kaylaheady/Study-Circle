eb4074effe15a274eb7c26cc54acdba1384949c6
kaylaheady <kaylaheady1@gmail.com>
2025-04-18 00:02:12 -0400

    Add new screens

--- supabase.js
+++ supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Replace these with your actual Supabase URL and Anon Key
 const SUPABASE_URL = "https://ssolcjnuddlznarnojud.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzb2xjam51ZGRsem5hcm5vanVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwNTk1MjcsImV4cCI6MjA1NjYzNTUyN30.kz2vUx7DAvbEzzOvff48o51z-wp4i23Sb9Jypt9Lnro";
